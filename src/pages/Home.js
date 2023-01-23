2a3fd71feba05e94baf3ccb7025f014f7d8cc519
NabilSaidani <96593015+NabilSaidani@users.noreply.github.com>
2023-01-23 13:04:05 +0100

    summary

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from "react"; 
 
-import {Layout} from "./../components/Layout.js"
+import {Layout} from "../components/Layout.js"
 
-export default function Home () {
+export const Home = () => {
     return <Layout>Home</Layout>
 }
+
+export default Home; 
+
+// export default function Home () {
+//     return <Layout>Home</Layout>
+// }
