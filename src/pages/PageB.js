a70ffc29c3590ec8a1c436161eeb031c01678476
NabilSaidani <96593015+NabilSaidani@users.noreply.github.com>
2023-01-23 12:19:56 +0100

    Summary
    J'ai modifier menu et ajouté goto, et j'ai ajouté un fichier .nvmrc

--- src/pages/PageB.js
+++ src/pages/PageB.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout } from "./../components/layout.js"
+import { Layout } from "../components/layout.js"
 
 export const PageB = () => {
   return <Layout><h1>Projet B</h1></Layout>;
